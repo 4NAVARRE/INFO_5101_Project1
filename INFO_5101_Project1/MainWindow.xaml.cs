99f553192035de8333fd67bbb391a2267faebbce
Kieran Primeau <k_primeau124250@fanshaweonline.ca>
2023-02-22 20:41:30 -0500

    fixed missing cs

--- INFO_5101_Project1/MainWindow.xaml.cs
+++ INFO_5101_Project1/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace INFO_5101_Project1
 {
@@ -22,6 +14,54 @@ namespace INFO_5101_Project1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void XML_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files", "Canadacities-XML.xml");
+            BackEnd.Statistics statistics = new(path, "xml");
+            Statistics statisticPage = new();
+            statisticPage.Show();
+        }
+
+        private void JSON_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files", "Canadacities-JSON.json");
+            BackEnd.Statistics statistics = new(path, "json");
+            Statistics statisticPage = new();
+            statisticPage.Show();
+        }
+        private void CSV_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files", "Canadacities.csv");
+            BackEnd.Statistics statistics = new(path, "csv");
+            Statistics statisticPage = new();
+            statisticPage.Show();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Hide_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Exit_MouseMove(object sender, MouseEventArgs e)
+        {
+            Exit.Background = new SolidColorBrush(Color.FromArgb(255, 132, 40, 40));
+        }
+
+        private void Hide_MouseMove(object sender, MouseEventArgs e)
+        {
+            Exit.Background = new SolidColorBrush(Color.FromArgb(255, 219, 49, 49));
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
         }
     }
 }
