998d50e07c7a8d01f4bb87e7ca35eccdc823fad9
Kieran Primeau <k_primeau124250@fanshaweonline.ca>
2023-02-22 19:41:48 -0500

    added agni files

---
+++ INFO_5101_Project1/Display_Province_Rank.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace INFO_5101_Project1
+{
+    /// <summary>
+    /// Interaction logic for Display_Province_Rank.xaml
+    /// </summary>
+    public partial class Display_Province_Rank : Window
+    {
+        public Display_Province_Rank()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- INFO_5101_Project1/obj/Debug/net6.0-windows/App.g.i.cs
+++ INFO_5101_Project1/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace INFO_5101_Project1 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace INFO_5101_Project1 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/INFO_5101_Project1;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/INFO_5101_Project1;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace INFO_5101_Project1 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public static void Main() {
             INFO_5101_Project1.App app = new INFO_5101_Project1.App();
             app.InitializeComponent();
