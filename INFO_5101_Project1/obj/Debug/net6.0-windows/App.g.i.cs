68de3878eccbbd1dbecaad26b1f5348e283af55e
Kieran Primeau <k_primeau124250@fanshaweonline.ca>
2023-02-22 21:00:43 -0500

    Fixed Main

--- INFO_5101_Project1/obj/Debug/net6.0-windows/App.g.i.cs
+++ INFO_5101_Project1/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace INFO_5101_Project1 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/INFO_5101_Project1;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/INFO_5101_Project1;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- INFO_5101_Project1/obj/Debug/net6.0-windows/Citys.g.i.cs
+++ INFO_5101_Project1/obj/Debug/net6.0-windows/Citys.g.i.cs
@@ -53,7 +53,7 @@ namespace INFO_5101_Project1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/INFO_5101_Project1;V1.0.0.0;component/citys.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/INFO_5101_Project1;component/citys.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Citys.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
